99f42a5d7f01f036da94d882dfff274a6da5b104
Team10477 <rapidrobotsnovi@gmail.com>
2021-03-12 19:56:03 -0500

    Add voltage sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RightRedTargetZones_Encoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RightRedTargetZones_Encoder.java
@@ -9,26 +9,12 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcore.external.ClassFactory;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
-import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
-import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
-
-import java.util.List;
-
 @Autonomous(name="Encoder-Red Right Target Zones")
 public class RightRedTargetZones_Encoder extends LinearOpMode {
-    private static final String TFOD_MODEL_ASSET = "UltimateGoal.tflite";
-    private static final String LABEL_QUAD_ELEMENT ="Quad";
-    private static final String LABEL_SINGLE_ELEMENT = "Single";
 
-    private static final String VUFORIA_KEY =
-            "AZvVY5H/////AAABmVIJC2J+Bk8dmED6q+xzKkUsIvfAZMxMAHooL0K1pItNOLCvp0Mq1vBnpcHiPjwlLYt5OOVpnoyE72D5Ku8iR1ahgCo9Le6ymYWRR/No45AHVGMbfpnYxmMchxYfGn/otWPMzvohNI2lyeJwmumFgHIO9nAJ7YH86HptFVWynm16S2ENaAUfBOeKxqPABdc7i8PCe7r/PMeY2gVSAp9/53S3Gi9PGvv4f2wCtlSQChSAsbdglIuy91gLnHHzR0w1itEjtRvSnronkqr/2P9xNfWcp8tN7duysWh6eQlUhAWerhD05mvmBorePfpmCfS2sWn4FTMdzR3vZZxypY1NgOx7OvYSEhPaapYcX8ydsBOb";
+    RingIdentification ringIdentification = new RingIdentification();
 
-    private VuforiaLocalizer vuforia;
-    private TFObjectDetector tfod;
-
-    //Target zone variables to keep hold of the number of rings: 1 (Target A),2 (Target B), 3 (Target C.
+     //Target zone variables to keep hold of the number of rings: 1 (Target A),2 (Target B), 3 (Target C.
     private int targetZone = 0;
 
     private ElapsedTime elapsedTime = new ElapsedTime();
@@ -66,11 +52,11 @@ public class RightRedTargetZones_Encoder extends LinearOpMode {
         waitForStart();
         int counter = 1;
         while (opModeIsActive() && counter == 1) {
-            identifyRings();
-            goToTargetZones();
+           targetZone =  ringIdentification.identifyRings(this.telemetry);
+           goToTargetZones();
             counter++;
         }
-        deactivateTfod();
+        ringIdentification.deactivateTfod();
     }
 
     public void initHardwareMap() {
@@ -92,9 +78,7 @@ public class RightRedTargetZones_Encoder extends LinearOpMode {
         hardwarePushBot.rightBackWheel.setDirection(DcMotor.Direction.FORWARD);
         hardwarePushBot.wobbleGoalFinger.setPosition(1);
         hardwarePushBot.shootingTrigger.setPosition(0);
-        initVuforia();
-        initTfod();
-        activateTfod();
+        ringIdentification.initVuforia(hardwareMap);
 
         telemetry.addData("Initialization complete", "Done");
         telemetry.update();
@@ -203,9 +187,16 @@ public class RightRedTargetZones_Encoder extends LinearOpMode {
 
     }
 
+    public void startShootingWheelByVoltage() {
+        double battVoltage= getVoltage();
+        hardwarePushBot.shootingWheel.setPower(0.748-(battVoltage-12)*(0.748-0.62)/(1.95));
+
+    }
+
     public void goToTargetZones() {
         switch (targetZone) {
-            case 1: //Target zone A
+            case 0: //Target zone A
+
                 //1. Strafe Right
 
                 stopResetEncoder();
@@ -233,10 +224,10 @@ public class RightRedTargetZones_Encoder extends LinearOpMode {
 
                 runToPositionForWobbleArm(0.75);
 
-              //  double battVoltage= getVoltage();
-
                 //5. Turn on shooting wheel.
-                hardwarePushBot.shootingWheel.setPower(0.70);
+              //  hardwarePushBot.shootingWheel.setPower(0.70);
+
+                startShootingWheelByVoltage();
 
                 runWithoutEncoder();
 
@@ -256,58 +247,8 @@ public class RightRedTargetZones_Encoder extends LinearOpMode {
                     sleep(500);
                 }
 
-                //9. Move forward to park.
-                //6. Go back 0.5 using feedback
-               // drivewWithFeedback_FBM(-0.5, 0, 0.25);
-
-                //Turn Right
-               /* stopResetEncoder();
-
-                setTargetPosition(TURN_RIGHT,2700);
-
-                runToPosition(1);
-
-                //Strafe Right
-
-                setTargetPosition(STRAFE_RIGHT,1000);
-
-                runToPosition(1);
-
-                //Move wobble arm down.
-                setTargetPosition(WOBBLE_ARM_DOWN, 3100);
-
-                runToPositionForWobbleArm(0.75);
-
-                hardwarePushBot.wobbleGoalFinger.setPosition(0);*/
-
-                //Drive forward.
-             /*   setTargetPosition(DRIVE_FORWARD,1800);
-
-                runToPosition(0.8);*/
-
-                //Drive forward with feedback.
-              /*  runWithoutEncoder();
-
-                drivewWithFeedback_FBM(-0.6, 0, 6);
-
-                //Close finger
-                hardwarePushBot.wobbleGoalFinger.setPosition(1);
-
-                sleep(1000);
-
-                stopResetEncoder();
-
-                //4. Wobble Goal Arm up
-                setTargetPosition(WOBBLE_ARM_UP, 3100);
-
-                runToPositionForWobbleArm(0.75);
-
-                //Turn Left
-                setTargetPosition(TURN_LEFT,2700);
-
-                runToPosition(1);*/
                 break;
-            case 2 :
+            case 1 : // Target zone B
 
                 //1. Go to the right
 
@@ -357,7 +298,8 @@ public class RightRedTargetZones_Encoder extends LinearOpMode {
                 drivewWithFeedback_FBM_Colors(0.5, 0, 3.0, "RED");
 
                 //5. Turn on shooting wheel.
-                hardwarePushBot.shootingWheel.setPower (0.7)    ;
+              //  hardwarePushBot.shootingWheel.setPower (0.7)    ;
+                startShootingWheelByVoltage();
 
                 //7. Strafe left using feedback
                 drivewWithFeedback_FBM(0, -0.5, 0.9);
@@ -372,7 +314,7 @@ public class RightRedTargetZones_Encoder extends LinearOpMode {
                     sleep(500);
                 }
                 break;
-            case 3:
+            case 4:  // Target zone C
 
                 //1. Go to the right
 
@@ -406,7 +348,8 @@ public class RightRedTargetZones_Encoder extends LinearOpMode {
                 runToPosition(1);
 
                 //5. Turn on shooting wheel.
-                hardwarePushBot.shootingWheel.setPower(0.7);
+             //   hardwarePushBot.shootingWheel.setPower(0.7);
+                startShootingWheelByVoltage();
 
                 runWithoutEncoder();
 
@@ -430,104 +373,6 @@ public class RightRedTargetZones_Encoder extends LinearOpMode {
                 //6. Go back 0.5 using feedback
                 drivewWithFeedback_FBM(-0.5, 0, 0.25);
                 break;
-        }
-    }
-
-    /**
-     * Initialize the Vuforia localization engine.
-     */
-    private void initVuforia() {
-        /*
-         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
-         */
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
-
-        parameters.vuforiaLicenseKey = VUFORIA_KEY;
-        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
-
-        //  Instantiate the Vuforia engine
-        vuforia = ClassFactory.getInstance().createVuforia(parameters);
-
-        // Loading trackables is not necessary for the TensorFlow Object Detection engine.
-    }
-
-    /**
-     * Initialize the TensorFlow Object Detection engine.
-     */
-    private void initTfod() {
-        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
-                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
-        tfodParameters.minResultConfidence = 0.8f;
-        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
-        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_QUAD_ELEMENT, LABEL_SINGLE_ELEMENT);
-    }
-
-    public void activateTfod() {
-        /**
-         * Activate TensorFlow Object Detection before we wait for the start command.
-         * Do it here so that the Camera Stream window will have the TensorFlow annotations visible.
-         **/
-        if (tfod != null) {
-            tfod.activate();
-
-            // The TensorFlow software will scale the input images from the camera to a lower resolution.
-            // This can result in lower detection accuracy at longer distances (> 55cm or 22").
-            // If your target is at distance greater than 50 cm (20") you can adjust the magnification value
-            // to artificially zoom in to the center of image.  For best results, the "aspectRatio" argument
-            // should be set to the value of the images used to create the TensorFlow Object Detection model
-            // (typically 16/9).
-            tfod.setZoom(2.5, 16.0/9.0);
-        }
-    }
-
-    /**
-     * Ring Identification.
-     */
-    public void identifyRings() {
-        if (tfod != null) {
-            // getUpdatedRecognitions() will return null if no new information is available since
-            // the last time that call was made.
-            List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
-            if (updatedRecognitions != null) {
-                telemetry.addData("# Object Detected", updatedRecognitions.size());
-                if (updatedRecognitions.size() == 0 ) {
-                    // empty list.  no objects recognized.
-                    telemetry.addData("TFOD", "No items detected.");
-                    telemetry.addData("Target Zone", "A");
-                    targetZone = 1;
-                } else {
-                    // list is not empty.
-                    // step through the list of recognitions and display boundary info.
-                    int i = 0;
-                    for (Recognition recognition : updatedRecognitions) {
-                        telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
-                        telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
-                                recognition.getLeft(), recognition.getTop());
-                        telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
-                                recognition.getRight(), recognition.getBottom());
-
-                        // check label to see which target zone to go after.
-                        if (recognition.getLabel().equals("Single")) {
-                            telemetry.addData("Target Zone", "B");
-                            targetZone = 2;
-                        } else if (recognition.getLabel().equals("Quad")) {
-                            telemetry.addData("Target Zone", "C");
-                            targetZone = 3;
-                        } else {
-                            telemetry.addData("Target Zone", "UNKNOWN");
-                        }
-                    }
-                }
-
-                telemetry.update();
-            }
-        }
-    }
-
-    public void deactivateTfod() {
-        if (tfod != null) {
-            tfod.shutdown();
         }
     }
 
